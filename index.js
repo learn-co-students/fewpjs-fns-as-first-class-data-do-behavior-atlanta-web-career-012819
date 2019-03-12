1c0d65dd14e4e14c4fe55367adde0c38300026c7
Megan Fago <meganlaynefago@gmail.com>
2019-03-12 10:01:59 -0400

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(strTime) {
+  let hour = strTime.split(":")[0]
+  console.log(hour)
+  if (hour < 12) {
+    return "Good Morning"
+  } else if (hour < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(greetingMsg){
+  const greeting = document.getElementById("greeting")
+  greeting.innerHTML = greetingMsg 
+}
